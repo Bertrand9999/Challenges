3f03eb7c623fd92f2639cd639d02efc5dae14a33
Bertrand9999 <130929311+Bertrand9999@users.noreply.github.com>
2023-05-24 05:54:54 +0200

    Update depenses.js

--- depenses.js
+++ depenses.js
@@ -8,22 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const expenseListElement = document.getElementById("expenseList");
 
   function addExpense() {
-    const expenseName = document.getElementById("expenseName").value;
-    const expenseAmount = parseFloat(document.getElementById("expenseAmount").value);
+  const expenseName = document.getElementById("expenseName").value;
+  const expenseAmount = parseFloat(document.getElementById("expenseAmount").value);
 
-    if (expenseName && expenseAmount) {
-      const expense = { name: expenseName, amount: expenseAmount };
-      expenseList.push(expense);
-      remainingAmount -= expenseAmount;
+  if (expenseName && expenseAmount) {
+    const expense = { name: expenseName, amount: expenseAmount };
 
-      updateRemainingAmount();
-      updateExpenseList();
-      saveToLocalStorage();
-      clearInputFields();
-    } else {
-      alert("Veuillez entrer un nom et un montant valide pour la dépense.");
-    }
+    // Envoyer la dépense à Firebase
+    db.collection('expenses').add(expense)
+      .then((docRef) => {
+        console.log('Dépense enregistrée avec l\'ID: ', docRef.id);
+        expenseList.push({ id: docRef.id, ...expense });
+        remainingAmount -= expenseAmount;
+        updateRemainingAmount();
+        updateExpenseList();
+      })
+      .catch((error) => {
+        console.error('Erreur lors de l\'ajout de la dépense: ', error);
+      });
+  } else {
+    alert("Veuillez entrer un nom et un montant valide pour la dépense.");
   }
+}
+
 
   function updateRemainingAmount() {
     remainingAmountElement.textContent = remainingAmount.toFixed(2);
@@ -46,13 +53,21 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function removeExpense(index) {
-    const removedExpense = expenseList.splice(index, 1)[0];
-    remainingAmount += removedExpense.amount;
+  const removedExpense = expenseList.splice(index, 1)[0];
+  remainingAmount += removedExpense.amount;
 
-    updateRemainingAmount();
-    updateExpenseList();
-    saveToLocalStorage();
-  }
+  // Supprimer la dépense de Firebase
+  db.collection('expenses').doc(removedExpense.id).delete()
+    .then(() => {
+      console.log('Dépense supprimée avec succès');
+      updateRemainingAmount();
+      updateExpenseList();
+    })
+    .catch((error) => {
+      console.error('Erreur lors de la suppression de la dépense: ', error);
+    });
+}
+
 
   function saveToLocalStorage() {
     localStorage.setItem("remainingAmount", remainingAmount);
