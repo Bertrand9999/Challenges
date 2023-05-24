fd6f61f0da858f4cda4a5aba44f960129d1a61a4
Bertrand9999 <130929311+Bertrand9999@users.noreply.github.com>
2023-05-24 05:53:05 +0200

    Update depenses.js

--- depenses.js
+++ depenses.js
@@ -1,158 +1,72 @@
 document.addEventListener("DOMContentLoaded", () => {
   const initialAmount = 400;
-  let remainingAmount = initialAmount;
-  let itemList = [];
-  let validatedExpensesList = [];
+  let remainingAmount =
+    parseFloat(localStorage.getItem("remainingAmount")) || initialAmount;
+  let expenseList = JSON.parse(localStorage.getItem("expenseList")) || [];
 
-  // Obtenir une référence à Firestore
-  var db = firebase.firestore();
+  const remainingAmountElement = document.getElementById("remainingAmount");
+  const expenseListElement = document.getElementById("expenseList");
 
-  function addItem() {
-    const itemName = document.getElementById("itemName").value;
-    const itemPrice = parseFloat(document.getElementById("itemPrice").value);
+  function addExpense() {
+    const expenseName = document.getElementById("expenseName").value;
+    const expenseAmount = parseFloat(document.getElementById("expenseAmount").value);
 
-    if (itemName && itemPrice && remainingAmount >= itemPrice) {
-      const item = { name: itemName, price: itemPrice };
-      
-      // Envoyer l'objet à Firestore
-      db.collection('depenses').add({
-          nom: item.name,
-          prix: item.price,
-          date: firebase.firestore.Timestamp.fromDate(new Date())
-      })
-      .then(function(docRef) {
-          console.log('Dépense enregistrée avec l\'ID: ', docRef.id);
-          item.id = docRef.id;
-          itemList.push(item);
-          remainingAmount -= itemPrice;
-          updateRemainingAmount();
-          updateItemList();
-      })
-      .catch(function(error) {
-          console.error('Erreur lors de l\'ajout de la dépense: ', error);
-      });
+    if (expenseName && expenseAmount) {
+      const expense = { name: expenseName, amount: expenseAmount };
+      expenseList.push(expense);
+      remainingAmount -= expenseAmount;
+
+      updateRemainingAmount();
+      updateExpenseList();
+      saveToLocalStorage();
+      clearInputFields();
     } else {
-      alert(
-        "Vérifiez le nom et le prix de l'élément, ou vérifiez si vous avez suffisamment d'argent restant."
-      );
+      alert("Veuillez entrer un nom et un montant valide pour la dépense.");
     }
   }
 
-function updateValidatedExpensesList() {
-  const validatedExpensesListElement = document.getElementById("validatedExpensesList");
-  validatedExpensesListElement.innerHTML = "";
-
-  validatedExpensesList.forEach((expense, index) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = `${expense.nom} - ${expense.prix} €`;
-    validatedExpensesListElement.appendChild(listItem);
-  });
-}
-
-
   function updateRemainingAmount() {
-    document.getElementById("remainingAmount").textContent = remainingAmount.toFixed(2);
-    document.getElementById("initialAmount").textContent = initialAmount.toFixed(2);
+    remainingAmountElement.textContent = remainingAmount.toFixed(2);
   }
 
-  function updateItemList() {
-    db.collection('depenses').get().then(function(querySnapshot) {
-      itemList = [];
-      querySnapshot.forEach(function(doc) {
-        const item = doc.data();
-        item.id = doc.id;
-        itemList.push(item);
-      });
+  function updateExpenseList() {
+    expenseListElement.innerHTML = "";
 
-      const itemListElement = document.getElementById("itemList");
-      itemListElement.innerHTML = "";
+    expenseList.forEach((expense, index) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${expense.name} - ${expense.amount} €`;
 
-      itemList.forEach((item, index) => {
-        const li = document.createElement("li");
-        li.textContent = `${item.nom} - ${item.prix} €`;
-        const removeButton = document.createElement("button");
-        removeButton.textContent = "Supprimer";
-        removeButton.onclick = () => removeItem(index);
-        li.appendChild(removeButton);
-        itemListElement.appendChild(li);
-      });
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "Supprimer";
+      removeButton.onclick = () => removeExpense(index);
+
+      listItem.appendChild(removeButton);
+      expenseListElement.appendChild(listItem);
     });
   }
 
-  function removeItem(index) {
-    const item = itemList[index];
-    db.collection('depenses').doc(item.id).delete()
-      .then(function() {
-        console.log('Dépense supprimée avec l\'ID: ', item.id);
-        remainingAmount += item.prix;
-        itemList.splice(index, 1);
-        updateRemainingAmount();
-        updateItemList();
-      })
-      .catch(function(error) {
-        console.error('Erreur lors de la suppression de la dépense: ', error);
-      });
+  function removeExpense(index) {
+    const removedExpense = expenseList.splice(index, 1)[0];
+    remainingAmount += removedExpense.amount;
+
+    updateRemainingAmount();
+    updateExpenseList();
+    saveToLocalStorage();
   }
 
-  function validateExpenses() {
-  if (itemList.length > 0) {
-    const batch = db.batch();
-
-    itemList.forEach((item) => {
-      const docRef = db.collection('validatedExpenses').doc();
-      batch.set(docRef, item);
-    });
-
-    batch.commit().then(() => {
-      console.log('Dépenses validées enregistrées avec succès');
-      remainingAmount -= itemList.reduce((total, item) => total + item.prix, 0);
-      itemList = [];
-      updateRemainingAmount();
-      updateItemList();
-      updateValidatedExpensesList();
-
-      // Mettre à jour la valeur du montant restant dans la base de données
-      db.collection('remainingAmount').doc('amount').set({ value: remainingAmount })
-        .then(() => {
-          console.log('Montant restant mis à jour avec succès');
-        })
-        .catch((error) => {
-          console.error('Erreur lors de la mise à jour du montant restant : ', error);
-        });
-    }).catch((error) => {
-      console.error('Erreur lors de l\'enregistrement des dépenses validées : ', error);
-    });
-  } else {
-    alert("Il n'y a pas d'éléments à valider.");
+  function saveToLocalStorage() {
+    localStorage.setItem("remainingAmount", remainingAmount);
+    localStorage.setItem("expenseList", JSON.stringify(expenseList));
   }
-}
 
+  function clearInputFields() {
+    document.getElementById("expenseName").value = "";
+    document.getElementById("expenseAmount").value = "";
+  }
 
-  const validateExpensesButton = document.getElementById("validateExpensesButton");
-  validateExpensesButton.addEventListener("click", validateExpenses);
+  const addExpenseButton = document.getElementById("addExpenseButton");
+  addExpenseButton.addEventListener("click", addExpense);
 
-  const addItemButton = document.getElementById("addItemButton");
-  addItemButton.addEventListener("click", addItem);
-
-  // Récupérer les données initiales depuis Firebase
-  db.collection('remainingAmount').doc('amount').get()
-    .then((doc) => {
-      if (doc.exists) {
-        remainingAmount = doc.data().value;
-        updateRemainingAmount();
-      }
-    });
-
-  // Récupérer les dépenses validées depuis Firebase
-  db.collection('validatedExpenses').get()
-    .then((querySnapshot) => {
-      validatedExpensesList = [];
-      querySnapshot.forEach((doc) => {
-        validatedExpensesList.push(doc.data());
-      });
-      updateValidatedExpensesList();
-    });
-
-  // Mettre à jour la liste des dépenses
-  updateItemList();
+  updateRemainingAmount();
+  updateExpenseList();
 });
